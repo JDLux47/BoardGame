45550e7f391cea3663e80d72f52b24a2b2ed1315
JDLux <JD921L@yandex.ru>
2023-08-21 21:35:52 +0300

    BG-4 Функции создания, отрисовки и сохранения карты. Текстуры.

--- Classes/Blocks.py
+++ Classes/Blocks.py
@@ -5,38 +5,35 @@ from pygame.locals import (
     RLEACCEL,
 )
 
+
 class Block: 
     
-    def __init__(self, x, y):
+    def __init__(self, x, y, name):
         self.x = x
         self.y = y
+        self.name = name
 
-    
+    def draw(self, screen):
+        self.image = pygame.image.load("Textures/" + self.name + ".png").convert()
+        # меняет размер изображения
+        self.scaled_image = pygame.transform.scale(self.image, (BLOCK_SIZE, BLOCK_SIZE))
+        screen.blit(self.scaled_image, (self.x * BLOCK_SIZE, self.y * BLOCK_SIZE))
+
+
+class Door(Block):
+
+    def __init__(self, x, y, name, open):
+        super().__init__(x, y, name) # берём атрибуты от родителя
+        self.open = open
+
+
 class Wall(Block):
 
-    # инициализация
-    def __init__(self, x, y):
-        # берёт х и у от родителя
-        super().__init__(x, y)
-        self.image = pygame.image.load("Textures/wall.jpg").convert()
-        # меняет размер изображения
-        self.scaled_image = pygame.transform.scale(self.image, (BLOCK_SIZE, BLOCK_SIZE))
-
-    # Функция рисует стену 
-    def draw(self, screen):
-        screen.blit(self.scaled_image, (self.x * BLOCK_SIZE, self.y * BLOCK_SIZE))
+    def __init__(self, x, y, name):
+        super().__init__(x, y, name)
 
 
-class Empty(Block):
-    
-    # инициализация
-    def __init__(self, x, y):
-        # берёт х и у от родителя
-        super().__init__(x, y)
-        self.image = pygame.image.load("Textures/empty.jpg").convert()
-        # меняет размер изображения
-        self.scaled_image = pygame.transform.scale(self.image, (BLOCK_SIZE, BLOCK_SIZE))
+class Floor(Block):
 
-    # Функция рисует пустое место 
-    def draw(self, screen):
-        screen.blit(self.scaled_image, (self.x * BLOCK_SIZE, self.y * BLOCK_SIZE))
+    def __init__(self, x, y, name):
+        super().__init__(x, y, name)
